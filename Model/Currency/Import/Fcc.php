1f7455a23a552046bdcb94c2c5e70276b5365df5
Stas Kamyshov <kamsd@ya.ru>
2020-10-30 15:42:36 +0300

    Update Fcc.php

--- Model/Currency/Import/Fcc.php
+++ Model/Currency/Import/Fcc.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Authors:
- *      krybbio <https://magento.stackexchange.com/users/15051/krybbio>
- *      Alex Gusev <alex@flancer64.com>
- * Since: 2018
- */
 
 namespace EHC\MagentoCurrencyCBR\Model\Currency\Import;
 
